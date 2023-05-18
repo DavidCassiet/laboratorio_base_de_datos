e9494f0a988f6483e3098fa659808077047a2a3e
David Cassiet <davidcassiet@gmail.com>
2023-05-18 18:49:29 -0300

    resubiendo archivo con cambios

--- laboratorio_bdd.sql
+++ laboratorio_bdd.sql
@@ -1,3 +1,23 @@
 use aceleracion;
 
 show tables;
+
+create table if not exists usuarios(
+	id BIGINT,
+	nombre VARCHAR(50),
+	apellido VARCHAR(50),
+	celular VARCHAR(30),
+	correo VARCHAR(50),
+	clave VARCHAR(50)
+);
+
+insert into usuarios (nombre, apellido, celular, correo, clave)
+				values("Leonel", "Sierra", "847956542", "leonelsierra@gmail.com", "iuo$89daczxp5");
+
+select * from usuarios;
+
+select * from usuarios where apellido like 'Magaña';
+
+update usuarios set celular = '587891332' where correo like 'rubenlopez@gmail.com';
+
+delete from usuarios where correo like 'leonelsierra@gmail.com';
